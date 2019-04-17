a8100ceb92f302d78936ccf53f1545433b5fe42c
Beta Anggoro <beta@getlinks.co>
2019-04-17 20:43:32 +0700

    Passport config

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
+const path = require('path')
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+// Passport Config
+require('./config/passport')(passport)
 
 const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/storybook', {
+  useNewUrlParser: true
+})
+
+// Load Routes
+const auth = require('./routes/auth')
 
 const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send('It Works')
 })
+
+app.use('/auth', auth)
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
