d590ae7b21e78d3bcc0c21386ed79a4f8067ec2e
Beta Anggoro <beta@getlinks.co>
2019-04-17 23:16:35 +0700

    Missing keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://benbkk:b3NN@10120@cluster0-qsyd8.mongodb.net/storybook?retryWrites=true',
+  googleClientID: '782111219314-ok3gic7q6njbric2g13akeogs2hsmbbf.apps.googleusercontent.com',
+  googleClientSecret: 'Tsz8ZaGwxlJYQjHC03DxIkpM'
+}
